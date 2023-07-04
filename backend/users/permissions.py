c70cd544b808879c7ef5633f65566a1c093088ef
Alexander <qrer71@yandex.ru>
2023-07-04 22:22:55 +0300

    Create custom permission

---
+++ backend/users/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+
+class IsAuthorPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user == obj.author and request.user.is_authenticated
+        )

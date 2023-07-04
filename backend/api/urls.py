41e6fead97a75b171293d130b0a5ae866ffc68b5
Alexander <qrer71@yandex.ru>
2023-07-04 21:57:14 +0300

    Connect apps to api

---
+++ backend/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from users.views import UserViewSet
+from tags.views import TagViewSet
+from ingredients.views import IngredientViewSet
+from recipes.views import RecipeViewSet
+
+
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet, basename='users')
+router_v1.register('tags', TagViewSet, basename='tags')
+router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router_v1.register('recipes', RecipeViewSet, basename='recipes')
+
+urlpatterns = [
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router_v1.urls)),
+]

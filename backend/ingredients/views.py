97cd6c429c22485b9870bd8bc5907ee8e24f0e5f
Alexander <qrer71@yandex.ru>
2023-07-04 22:19:34 +0300

    Create ViewSet

---
+++ backend/ingredients/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, permissions
+
+from .models import Ingredient
+from .serializers import IngredientSerializer
+from core.filters import BackendSearchFilter
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (permissions.AllowAny,)
+    filter_backends = (BackendSearchFilter,)
+    search_fields = ('$name',)

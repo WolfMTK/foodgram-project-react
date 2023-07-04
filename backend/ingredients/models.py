b9cb779a6674d564ac2f179f923f30921306a1c8
Alexander <qrer71@yandex.ru>
2023-07-04 22:19:03 +0300

    Create model Ingredient

---
+++ backend/ingredients/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Ingredient(models.Model):
+    """Модель ингредиентов."""
+
+    name = models.CharField(max_length=200, verbose_name='Название')
+    measurement_unit = models.CharField(
+        max_length=200,
+        verbose_name='Единицы измерения',
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name

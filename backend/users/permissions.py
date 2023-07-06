e8a13605784b26313026a69dcfafc4030ed42f4b
Alexander <qrer71@yandex.ru>
2023-07-06 11:44:45 +0300

    Update permissions.py

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -9,4 +9,3 @@ class IsAuthorPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.author and request.user.is_authenticated
-        

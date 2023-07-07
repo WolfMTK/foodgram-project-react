e202bad79face5fcefb5fa9b16916c2980277b87
Alexander <qrer71@yandex.ru>
2023-07-07 15:09:26 +0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -12,6 +12,8 @@ class UserViewSet(DjoserViewSet):
 
     pagination_class = PageNumberAndLimit
 
+    # Переопределил метод из-за GET запроса,
+    # Или не нужно было так делать?
     @decorators.action(detail=False, methods=('get',))
     def me(self, request, *args, **kwargs):
         return response.Response(
@@ -29,13 +31,14 @@ class UserViewSet(DjoserViewSet):
 
     @decorators.action(detail=True, methods=('post', 'delete'))
     def subscribe(self, request, id=None, *args, **kwargs):
+        subscribed = get_object_or_404(User, id=id)
         if request.method == 'POST':
             return response.Response(
-                self._get_subscribe_data(request, id, *args, **kwargs),
+                self._get_subscribe_data(request, subscribed, *args, **kwargs),
                 status.HTTP_201_CREATED,
             )
         subscription = Subscription.objects.filter(
-            user_id=request.user.id, subscribed_id=id
+            user=request.user, subscribed=subscribed
         )
         if not subscription.exists():
             return response.Response(
@@ -58,8 +61,7 @@ class UserViewSet(DjoserViewSet):
         return super().get_serializer_class()
 
     # Или всё же в subscribe это добавить, а не отдельным методом?
-    def _get_subscribe_data(self, request, pk=None, *args, **kwargs):
-        subscribed = get_object_or_404(User, id=pk)
+    def _get_subscribe_data(self, request, subscribed, *args, **kwargs):
         serializer = self.get_serializer(
             data={
                 'user': request.user,

45b1dc4b03499c761545c20c32265852235e6be6
Alexander <qrer71@yandex.ru>
2023-07-10 23:54:40 +0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.views import CustomUserViewSet
-from ingredients.views import IngredientViewSet
-from recipes.views import RecipeViewSet, TagViewSet
+from recipes.views import RecipeViewSet, TagViewSet, IngredientViewSet
 
 
 router_v1 = DefaultRouter()

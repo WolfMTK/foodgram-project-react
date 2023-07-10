4a0a830a1f5e2b7d3cb2cb4d0858c2d71544ae1c
Alexander <qrer71@yandex.ru>
2023-07-10 19:22:43 +0300

    Update load_data.py

--- backend/ingredients/management/commands/load_data.py
+++ backend/ingredients/management/commands/load_data.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from csv import DictReader
+from csv import reader
 
 from django.core.management import BaseCommand
 
@@ -14,18 +14,27 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     """Команда загрузки данных в модель ингредиенты."""
 
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('-p', '--path', type=str, help='Path to data')
+
     def handle(self, *args, **options):
-        if not Path('./data').is_dir():
-            msg = 'Ошибка! Не найдена папка data'
+        path = options.get('path')
+        if not path or not Path(path).is_dir():
+            msg = 'Ошибка! Не найдена папка data!'
             logger.error(msg)
             raise PathDirError(msg)
         self.stdout.write('Загрузка данных: ингредиенты!')
+        self._load_data(path)
+        self.stdout.write('Загрузка данных завершена!')
+
+    def _load_data(self, path):
         with open(
-            './data/ingredients.csv', mode='r', encoding='utg-8'
+            f'{path}ingredients.csv', mode='r', encoding='utf-8'
         ) as file:
-            for row in DictReader(file):
+            read_file = reader(file)
+            if next(read_file) in ['name', 'measurement_unit']:
+                file = read_file
+            for row in reader(file):
                 Ingredient.objects.get_or_create(
-                    name=row.get('name'),
-                    measurement_unit=row.get('measurement_unit'),
+                    name=row[0], measurement_unit=row[1]
                 )
-            file.close()

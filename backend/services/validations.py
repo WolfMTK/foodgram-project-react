2ee92ff4ba3629450d4d2f44ce6d4976631552e8
Alexander <qrer71@yandex.ru>
2023-07-10 14:27:13 +0300

    Update validations.py

--- backend/services/validations.py
+++ backend/services/validations.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from django.core import exceptions
+from django.conf import settings
 from django.contrib.auth import password_validation as validation
 
 
 def validate_password(password, key='password'):
-    if len(password) < 6:
+    if len(password) < settings.LEN_PASSWORD:
         raise serializers.ValidationError(
             {'error': 'Пароль меньше 6 символов!'}
         )

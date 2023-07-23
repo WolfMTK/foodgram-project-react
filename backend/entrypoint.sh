9aad5c10c846bf24ac0f55005dde6d5501c21495
Alexander <qrer71@yandex.ru>
2023-07-23 23:10:43 +0300

    Create entrypoint

---
+++ backend/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+python manage.py migrate --noinput || exit 1
+python manage.py collectstatic --noinput || exit 1
+cp -r /app/collected_static/. /backend_static/static/ || exit 1
+rm -f /app/entrypoint.sh || exit 1
+exec "$@"

1c499f65a96dfff1cf846916b66193760945ce68
Alexander <qrer71@yandex.ru>
2023-07-24 14:14:13 +0300

    Update file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+sleep 5s
 python manage.py migrate --noinput || exit 1
 python manage.py collectstatic --noinput || exit 1
 cp -r /app/collected_static/. /backend_static/static/ || exit 1
